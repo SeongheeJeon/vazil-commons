da1ecb0c2757e90db4dc8064ea2fce2a11ecae62
DESKTOP-VRLV5Q2\shj <shj@vazilcompany.com>
2023-03-24 13:44:47 +0900

    [ADD] VazilLayout 반응형 스타일 적용 WEB-52

--- src/components/VazilLayout.vue
+++ src/components/VazilLayout.vue
@@ -1,194 +1,201 @@
 <template>
   <v-app class="my-layout-container"> 
     <!-- 헤더 -->
-    <v-app-bar flat :height="headerHeight" >
+    <v-app-bar flat :height="headerHeight" :class="xs ? 'mobile' : ''">
       <v-img
+        v-if="xs || lgAndUp"
         :src="require('@/assets/' + (theme.global.name.value === 'dark' ? darkThemeLogo : lightThemeLogo))"
         :width="logoWidth"
         :maxWidth="logoWidth"
       />
-      <v-spacer></v-spacer>
 
-      <slot name="headerItem" :btnOptions="headerItemOptions"/>
-
-      <!-- 테마(light/dark) -->
-      <v-btn
-        v-if="headerActiveList.indexOf('theme') > -1"
-        v-bind="headerItemOptions"
-        @click="theme.global.name.value = theme.global.name.value === 'light' ? 'dark' : 'light'"
-      >
-        <v-icon>mdi-theme-light-dark</v-icon>
-      </v-btn>
-
-      <!-- 언어 -->
-      <div
-        v-if="headerActiveList.indexOf('locale') > -1"
-        class="locale-wrap"
-        v-click-outside="onClickOutsideLocale"
-      >
-        <v-btn
-          v-bind="headerItemOptions"
-          @click="localeActive = !localeActive"
+      <template v-slot:prepend v-if="mdAndDown">
+        <v-app-bar-nav-icon class="mr-4" @click="navDrawer = !navDrawer"></v-app-bar-nav-icon>
+      </template>
+      
+      <template v-slot:append v-if="xs">
+        <v-btn @click="headerMenuOn = !headerMenuOn" icon="mdi-dots-vertical" class="header-menu-btn"></v-btn>
+      </template>
+      
+      <!-- 헤더 메뉴 -->
+      <v-expand-transition>
+        <div
+          v-show="headerMenuOn"
+          class="header-menus"
+          v-click-outside="{handler: onClickOutsideHeaderMenu, include: headerMenuBtn}"
         >
-          <v-icon :color="localeActive ? 'primary':''">mdi-web</v-icon>
-        </v-btn>
+          <slot name="headerItem" :btnOptions="headerItemOptions"/>
 
-        <v-slide-y-transition origin="top right 0">
-          <v-card
-            v-if="localeActive"
-            class="mx-0 pa-0 locale-content"
+          <!-- 테마(light/dark) -->
+          <v-btn
+            v-if="headerActiveList.indexOf('theme') > -1"
+            v-bind="headerItemOptions"
+            @click="theme.global.name.value = theme.global.name.value === 'light' ? 'dark' : 'light'"
           >
-            <v-list>
-              <v-list-item
-                v-for="(item, index) in localeList" :key="index"
-                @click="changeLocale"
+            <v-icon>mdi-theme-light-dark</v-icon>
+          </v-btn>
+
+          <!-- 언어 -->
+          <div
+            v-if="headerActiveList.indexOf('locale') > -1"
+            class="locale-wrap"
+            v-click-outside="onClickOutsideLocale"
+          >
+            <v-btn
+              v-bind="headerItemOptions"
+              @click="localeActive = !localeActive"
+            >
+              <v-icon :color="localeActive ? 'primary':''">mdi-web</v-icon>
+            </v-btn>
+
+            <v-slide-y-transition origin="top right 0">
+              <v-card
+                v-if="localeActive"
+                class="mx-0 pa-0 locale-content"
               >
-                <span>{{item.text}}</span>
-              </v-list-item>
-            </v-list>
-          </v-card>
-        </v-slide-y-transition>
-      </div>
-      
-      <!-- 알림 -->
-      <div
-        v-if="headerActiveList.indexOf('notice') > -1"
-        class="notice-wrap"
-        v-click-outside="onClickOutsideNotice"
-      >
-        <!-- activator -->
-        <v-btn
-          v-bind="headerItemOptions"
-          @click="noticeActive = !noticeActive"
-          stacked
-        >
-          <v-badge
-            :model-value="unreadNotice.length > 0"
-            :content="unreadNotice.length"
-            max="99"
-            color="primary"
-          >
-            <v-icon :color="noticeActive ? 'primary':''">
-              mdi-bell{{!noticeActive ? '-outline' : ''}}
-            </v-icon>
-          </v-badge>
+                <v-list>
+                  <v-list-item
+                    v-for="(item, index) in localeList" :key="index"
+                    @click="changeLocale"
+                  >
+                    <span>{{item.text}}</span>
+                  </v-list-item>
+                </v-list>
+              </v-card>
+            </v-slide-y-transition>
+          </div>
           
-          <v-tooltip
-            activator="parent"
-            location="bottom"
-            text="알림"
-          />
-        </v-btn>
-
-        <!-- 알림 패널 -->
-        <v-slide-y-transition origin="top right 0">
-          <v-card
-            v-if="noticeActive"
-            class="mx-0 pa-0 notice-content"
-            
+          <!-- 알림 -->
+          <div
+            v-if="headerActiveList.indexOf('notice') > -1"
+            class="notice-wrap"
+            v-click-outside="onClickOutsideNotice"
           >
-            <!-- 커스텀 패널 -->
-            <div v-if="$slots.notificationPannel">
-              <slot name="notificationPannel" />
-            </div>
+            <!-- activator -->
+            <v-btn
+              v-bind="headerItemOptions"
+              @click="noticeActive = !noticeActive"
+              stacked
+            >
+              <v-badge
+                :model-value="unreadNotice.length > 0"
+                :content="unreadNotice.length"
+                max="99"
+                color="primary"
+              >
+                <v-icon :color="noticeActive ? 'primary':''">
+                  mdi-bell{{!noticeActive ? '-outline' : ''}}
+                </v-icon>
+              </v-badge>
+            </v-btn>
 
-            <!-- 기본 패널 -->
-            <div v-else>
-              <v-card-title class="d-flex align-center">
-                <span>알림</span> 
-                <v-spacer></v-spacer>
-                <v-btn
-                  size="x-small"
-                  plain
-                  text
-                  :disabled="unreadNotice.length === 0"
-                  :ripple="false"
-                >
-                  모두 읽음
-                </v-btn>
-              </v-card-title>
-              <v-divider/>
-              <v-card-text>
-                <v-row v-if="noticeLoading" no-gutters style="height:100%;" justify="center" align="center">
-                  <v-progress-circular indeterminate/>
-                </v-row>
+            <!-- 알림 패널 -->
+            <v-slide-y-transition origin="top right 0">
+              <v-card
+                v-if="noticeActive"
+                class="mx-0 pa-0 notice-content"
+                
+              >
+                <!-- 커스텀 패널 -->
+                <div v-if="$slots.notificationPannel">
+                  <slot name="notificationPannel" />
+                </div>
 
-                <v-row v-else no-gutters style="height:100% !important;">
-                  <v-list v-if="unreadNotice && unreadNotice.length > 0" style="width: 100%;" lines="2">
-                    <v-list-item
-                      v-for="(item, i) in unreadNotice"
-                      :key="i"
+                <!-- 기본 패널 -->
+                <div v-else>
+                  <v-card-title class="d-flex align-center">
+                    <span>알림</span> 
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      size="x-small"
+                      plain
+                      text
+                      :disabled="unreadNotice.length === 0"
                       :ripple="false"
-                      :inactive="true"
-                      :prepend-icon="item.type === 'WARNING' ? 'mdi-message-alert-outline' : 'mdi-message-processing-outline'"
-                      :subtitle="item.message"
                     >
-                      <!-- <template v-slot:title>
-                        <span>{{item.message}}</span>
-                      </template> -->
-                      
-                      <template v-slot:append>
-                        <span class="text-caption">{{$utils.getTimeFromNow(item.createdAt)}}</span>
-                      </template>
-                    </v-list-item>
-                  </v-list>
-                  <v-col v-else cols="12" no-gutters align="center" justify="center" class="d-flex justify-center align-center">
-                    <div>
-                      <v-icon style="opacity:0.7;" large class="mb-4">mdi-alert-circle-outline</v-icon>
-                      <br/>
-                      <div>새로운 알림이 없습니다.</div>
-                    </div>
-                  </v-col>
-                </v-row>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn block small text :ripple="false" style="text-transform: none" @click="goAlertPage()">전체 보기</v-btn>
-              </v-card-actions>
-            </div>
-          </v-card>
-        </v-slide-y-transition>
-      </div>
-      
-      <!-- 사용자 프로필 -->
-      <div
-        v-if="headerActiveList.indexOf('profile') > -1"
-        class="profile-wrap"
-        v-click-outside="onClickOutsideProfile"
-      >
-        <!-- activator -->
-        <v-btn
-          v-bind="headerItemOptions"
-          @click="profileActive = !profileActive"
-        >
-          <v-icon :color="profileActive ? 'primary':''">mdi-account</v-icon>
-        </v-btn>
+                      모두 읽음
+                    </v-btn>
+                  </v-card-title>
+                  <v-divider/>
+                  <v-card-text>
+                    <v-row v-if="noticeLoading" no-gutters style="height:100%;" justify="center" align="center">
+                      <v-progress-circular indeterminate/>
+                    </v-row>
 
-        <!-- 사용자 프로필 패널 -->
-        <v-slide-y-transition origin="top right 0">
-          <v-card
-            v-if="profileActive"
-            class="mx-0 pa-0 profile-content"
+                    <v-row v-else no-gutters style="height:100% !important;">
+                      <v-list v-if="unreadNotice && unreadNotice.length > 0" style="width: 100%;" lines="2">
+                        <v-list-item
+                          v-for="(item, i) in unreadNotice"
+                          :key="i"
+                          :ripple="false"
+                          :inactive="true"
+                          :prepend-icon="item.type === 'WARNING' ? 'mdi-message-alert-outline' : 'mdi-message-processing-outline'"
+                          :subtitle="item.message"
+                        >
+                          <template v-slot:append v-if="!xs">
+                            <span class="text-caption">{{$utils.getTimeFromNow(item.createdAt)}}</span>
+                          </template>
+                        </v-list-item>
+                      </v-list>
+                      <v-col v-else cols="12" no-gutters align="center" justify="center" class="d-flex justify-center align-center">
+                        <div>
+                          <v-icon style="opacity:0.7;" large class="mb-4">mdi-alert-circle-outline</v-icon>
+                          <br/>
+                          <div>새로운 알림이 없습니다.</div>
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn block small text :ripple="false" style="text-transform: none" @click="goAlertPage()">전체 보기</v-btn>
+                  </v-card-actions>
+                </div>
+              </v-card>
+            </v-slide-y-transition>
+          </div>
+          
+          <!-- 사용자 프로필 -->
+          <div
+            v-if="headerActiveList.indexOf('profile') > -1"
+            class="profile-wrap"
+            v-click-outside="onClickOutsideProfile"
           >
-            <!-- 커스텀 패널 -->
-            <div v-if="$slots.profilePannel">
-              <slot name="profilePannel" />
-            </div>
+            <!-- activator -->
+            <v-btn
+              v-bind="headerItemOptions"
+              @click="profileActive = !profileActive"
+            >
+              <v-icon :color="profileActive ? 'primary':''">mdi-account</v-icon>
+            </v-btn>
 
-            <!-- 기본 패널 -->
-            <div v-else>
-              <v-row no-gutters class="ma-2 pa-4 user-info" align="center" style="cursor:pointer;">
-                <v-col cols="3">
-                  <img v-if="userAvatar !== ''" :src="require('@/assets/' + userAvatar)" height="28" class="mr-2"/>
-                </v-col>
-                <v-col cols="9">
-                  <p class="ma-0">{{userName}}</p>
-                  <p class="ma-0">{{userEmail}}</p>
-                </v-col>
-              </v-row>
-            </div>
-          </v-card>
-        </v-slide-y-transition>
-      </div>
+            <!-- 사용자 프로필 패널 -->
+            <v-slide-y-transition origin="top right 0">
+              <v-card
+                v-if="profileActive"
+                class="mx-0 pa-0 profile-content"
+              >
+                <!-- 커스텀 패널 -->
+                <div v-if="$slots.profilePannel">
+                  <slot name="profilePannel" />
+                </div>
+
+                <!-- 기본 패널 -->
+                <div v-else>
+                  <v-row no-gutters class="ma-2 pa-4 user-info" align="center" style="cursor:pointer;">
+                    <v-col cols="3">
+                      <img v-if="userAvatar !== ''" :src="require('@/assets/' + userAvatar)" height="28" class="mr-2"/>
+                    </v-col>
+                    <v-col cols="9">
+                      <p class="ma-0">{{userName}}</p>
+                      <p class="ma-0">{{userEmail}}</p>
+                    </v-col>
+                  </v-row>
+                </div>
+              </v-card>
+            </v-slide-y-transition>
+          </div>
+        </div>
+      </v-expand-transition>
     </v-app-bar>
     
     <!-- nav -->
@@ -196,6 +203,7 @@
       app
       :rail="rail"
       rail-width="50"
+      v-model="navDrawer"
     >
       <v-row class="nav-container" no-gutters>
         <!-- nav list -->
@@ -243,11 +251,14 @@
 
 <script setup>
 import { ref, defineProps, onMounted, useSlots} from 'vue'
-import {useTheme} from "vuetify"
+import {useTheme, useDisplay} from "vuetify"
 const theme = useTheme()
 const slots = useSlots()
+const {xs, mdAndDown, lgAndUp} = useDisplay()
+
 
 // data
+const headerMenuOn = ref(!xs.value)
 const headerItemOptions = ref({
   icon: true,
   ripple: false,
@@ -280,6 +291,7 @@ const localeList = ref([
     text: 'English',
   },
 ])
+const navDrawer = ref(true)
 const rail = ref(true)
 const selectedNav = ref('home')
 
@@ -353,7 +365,12 @@ const onClickOutsideProfile = () => {
 const onClickOutsideLocale = () => {
   localeActive.value = false
 }
-
+const onClickOutsideHeaderMenu = () => {
+  if(xs.value) headerMenuOn.value = false
+}
+const headerMenuBtn = () => {
+  return [document.querySelector('.header-menu-btn')]
+}
 const changeLocale = () => {
   console.log('change locale : not yet implemented')
 }
@@ -384,81 +401,141 @@ const changeRail = () => {
     overflow: visible;
     padding: 4px 16px;
     
-    .locale-wrap {
-      position: relative;
+    &:deep(.v-toolbar__prepend), &:deep(.v-toolbar__append) {
+      margin: 0;
+      position: absolute;
 
-      .locale-content {
-        min-width: 140px;
-        width: 140px !important;
-        position: absolute;
-        right: 50%;
-        transform: translateX(50%);
-        border-radius: 10px !important;
-        margin-top: 16px;
-
-        .v-list-item {
-          text-align: center;
-        }
+      button {
+        width: 24px;
+        height: 24px;
       }
     }
     
-    .notice-wrap {
-      position: relative;
+    &:deep(.v-toolbar__prepend) {
+      left: 0;
+    }
+    
+    &:deep(.v-toolbar__append) {
+      right: 0;
+    }
 
-      .notice-content {
-        min-width: 380px;
-        width: 380px !important;
-        position: absolute;
-        right: 0px;
-        border-radius: 10px !important;
-        margin-top: 16px;
-        box-shadow: rgba(0, 0, 0, 0.05) 0px 20px 30px !important;
+    .header-menus {
+      display: flex;
+      position: absolute;
+      right: 0;
+      
+      .locale-wrap {
+        position: relative;
 
-        .v-card__text {
-          height: 320px;
-          overflow-y: auto;
-        }
+        .locale-content {
+          min-width: 140px;
+          width: 140px !important;
+          position: absolute;
+          right: 50%;
+          transform: translateX(50%);
+          border-radius: 10px !important;
+          margin-top: 16px;
 
-        .v-list {
-          height: 100%;
-          
-          &:deep(.v-icon) {
-            opacity: 1;
+          .v-list-item {
+            text-align: center;
           }
-          
-          &:deep(.v-list-item-subtitle){
-            opacity: 1;
-            line-height: 1.2rem;
+        }
+      }
+      
+      .notice-wrap {
+        position: relative;
+
+        .notice-content {
+          width: 380px !important;
+          position: absolute;
+          right: 0px;
+          border-radius: 10px !important;
+          margin-top: 16px;
+          box-shadow: rgba(0, 0, 0, 0.05) 0px 20px 30px !important;
+
+          .v-card__text {
+            height: 320px;
+            overflow-y: auto;
+          }
+
+          .v-list {
+            height: 100%;
+            
+            &:deep(.v-icon) {
+              opacity: 1;
+            }
+            
+            &:deep(.v-list-item-subtitle){
+              opacity: 1;
+              line-height: 1.2rem;
+            }
+          }
+        }
+      }
+
+      .profile-wrap {
+        position: relative;
+        
+        .profile-content {
+          position: absolute;
+          margin-top: 16px;
+          right: 0px;
+          border-radius: 10px !important;
+          width: 300px;
+
+          .user-info {
+            div:first-child {
+              text-align: center;
+            }
+
+            div:last-child {
+              p:nth-child(1) {
+                font-size: 1.1rem;
+                font-weight: 600;
+              }
+
+              p:nth-child(2) {
+                font-size: 0.9rem;
+                opacity: 0.8 !important;
+              }
+            }
           }
         }
       }
     }
 
-    .profile-wrap {
-      .profile-content {
-        position: absolute;
-        margin-top: 16px;
-        right: 0px;
-        border-radius: 10px !important;
-        width: 300px;
-        min-width: 300px !important;
+    &.mobile {
+      &:deep(.v-toolbar__content) {
+        justify-content: center;
+      }
 
-        .user-info {
-          div:first-child {
-            text-align: center;
-          }
-
-          div:last-child {
-            p:nth-child(1) {
-              font-size: 1.1rem;
-              font-weight: 600;
-            }
-
-            p:nth-child(2) {
-              font-size: 0.9rem;
-              opacity: 0.8 !important;
-            }
-          }
+      .header-menus {
+        background-color: rgba(var(--v-theme-surface), 1);
+        border-radius: 10px;
+        right: -6px;
+        flex-direction: column;
+        top: 50px;
+        
+        &:deep(> button), &:deep(> div) {
+          margin: 8px 0;
+        }
+        
+        .locale-content {
+          top: -16px;
+          right: 48px;
+          transform: none;
+        }
+        
+        .notice-content {
+          top: -16px;
+          right: 48px;
+          max-width: calc(100vw - 100px);
+        }
+        
+        .profile-content {
+          top: -16px;
+          right: 48px;
+          max-width: calc(100vw - 100px);
         }
       }
     }
