25cf0a6c012232b8ebedd84f76f6c63c6d91e8f8
전성희 <c960410@naver.com>
2023-04-18 17:39:20 +0900

    [ADD] VazilLogin 사용 테스트 WEB-92

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-    <span class="mr-4">{{$t('홈 화면')}}</span>
-    <VazilDialog
+    <!-- <span class="mr-4">{{$t('홈 화면')}}</span> -->
+    <!-- <VazilDialog
       ref="testDialog"
       v-bind="testDialogProps"
       @click-yes="clickYes"
       @click-no="clickNo"
     >
-      <!-- <template #loadBtn>
+      <template #loadBtn>
         <v-btn @click="$refs.testDialog.dialog = true">
           <v-icon>mdi-earth</v-icon>
         </v-btn>
-      </template> -->
+      </template>
       
-      <!-- <template #title>
+      <template #title>
         <v-row no-gutters>
           TITLE!!!
         </v-row>
-      </template> -->
+      </template>
       
       <template #content>
         <v-row no-gutters>
@@ -28,14 +28,14 @@
         </v-row>
       </template>
       
-      <!-- <template #actionBtn>
+      <template #actionBtn>
         <v-row no-gutters>
           <v-spacer></v-spacer>
           <v-btn>버튼</v-btn>
           <v-btn>안녕</v-btn>
         </v-row>
-      </template> -->
-    </VazilDialog>
+      </template>
+    </VazilDialog> -->
     
     <!-- <VazilPagination
       type="omitted"
@@ -60,23 +60,14 @@
       @set-page="setPage"
     /> -->
     
-    <!-- <div style="background-color: white; width: 440px;" class="mx-auto my-4"> -->
-      <VazilLogin
-        color="green"
-        class="mt-4"
-        width="400"
-      >
-        <!-- <template #loadBtn>
-          <v-btn @click="$refs.testDialog.dialog = true">
-            <v-icon>mdi-earth</v-icon>
-          </v-btn>
-        </template> -->
-      </VazilLogin>
-    <!-- </div> -->
+    <VazilLogin v-bind="vazilLoginProps" />
   </div>
 </template>
 
 <script>
+import privacy from '../assets/privacy.js';
+import policy from '../assets/policy.js';
+
 export default {
   name: 'HomeView',
   
@@ -95,6 +86,34 @@ export default {
         mobileFull: true,
       },
       page: 1,
+      privacy: privacy,
+      policy: policy,
+      vazilLoginProps: {
+        bgColor: "beige",
+        mainColor: "red",
+        class: "mt-4",
+        width: "500",
+        snsLoginList: ['naver', 'kakao', 'google'],
+        agreementList: [
+          {
+            key: 'fourteen',
+            title: '만 14세 이상입니다.',
+            content: null,
+            required: true,
+          },{
+            key: 'privacy',
+            title: '개인정보 수집 및 이용 동의',
+            content: privacy,
+            required: true,
+          },{
+            key: 'policy',
+            title: '이용약관 동의',
+            content: policy,
+            required: true,
+          }
+        ],
+        signUpInputList: ['email', 'password', 'nickname', 'captcha'],
+      }
     }
   },
   
